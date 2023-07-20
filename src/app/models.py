ae32335bc0dac428a5cb6374bb4748c7020d31f8
CieloMV <cmezav@unsa.edu.pe>
2023-07-20 12:59:24 -0500

    migrated tables & tabla principal correciones

--- src/app/models.py
+++ src/app/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from config import settings
+from django.contrib.auth.models import User
 
-class Chicharrones_LaJulia:
+
+class Chicharrones_LaJulia(models.Model):
     # aumentarle mas códigos para enlazar con otras tablas
-    cod_ruc = models.ForeignKey()                   #key
+    cod_ruc = models.ForeignKey(User, on_delete=models.CASCADE)     #key
     direccion = models.CharField(max_length=300)
     telefono =  models.IntegerField()
 
